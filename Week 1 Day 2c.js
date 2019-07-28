c0d899cf3966775dc5b0809f4b5639af8299e4ec
dipadana <putudipadana@gmail.com>
2019-07-28 08:04:32 +0700

    add several thig

---
+++ Week 1 Day 2c.js
@@ -0,0 +1,17 @@
+/*
+var groceryItem = 'papaya';
+
+switch (groceryItem) {
+case 'tomato':
+    console.log('Tomatoes are $0.49');
+    break;
+case 'lime':
+    console.log('Limes are $1.49');
+    break;
+case 'papaya':
+    console.log('Papayas are $1.29');
+    break;
+default:
+    console.log('Invalid item');
+    break;
+}*/
