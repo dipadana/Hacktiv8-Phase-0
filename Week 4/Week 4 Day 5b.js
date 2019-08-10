848aae6cdaa8aae5c7e9aaa775dee4395cd92dc1
dipadana <putudipadana@gmail.com>
2019-08-10 22:57:04 +0700

    enchance littel favor

--- Week 4/Week 4 Day 5b.js
+++ Week 4/Week 4 Day 5b.js
@@ -33,7 +33,7 @@ function naikAngkot(arr) {
 }
 
 //TEST CASE
-console.log(naikAngkot([['Dimitri', 'B', 'F'], ['Icha', 'A', 'B']]));
+console.log(naikAngkot([['Dimitri', 'B', 'F'], ['Icha', 'A', 'B'], ['Dipa', 'A', 'F']]));
 // [ { penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 },
 //   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
 
